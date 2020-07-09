36fbde129ffb1ac25efc16601bfa4987cda5fdfa
Franketo <franco.accifonte@gmail.com>
2020-07-08 21:20:56 -0300

    Add proyect basic files

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ```
 python3 -m venv env/
 source env/bin/activate
+.\env\Scripts\Activate.ps1
 pip3 install -r requirements.txt
 deactivate
 ```
---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import io
+import requests
+from time import sleep
+
+def main():
+    print("Hello mofo")
+
+if __name__ == "__main__":
+    main()
