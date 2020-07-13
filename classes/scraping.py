a997a5b269dc6d3ff1808dd9779b37fb03f34ca3
Franketo <franco.accifonte@gmail.com>
2020-07-12 23:43:08 -0300

    appends 20 first results url to a text file

--- classes/scraping.py
+++ classes/scraping.py
@@ -12,5 +12,11 @@ class LNBot:
         self.driver.find_element_by_xpath('//*[@id="JOBS"]/section[1]/input').send_keys(query)
         self.driver.find_element_by_xpath('//*[@id="JOBS"]/section[2]/input').clear()
         self.driver.find_element_by_xpath('//*[@id="JOBS"]/section[2]/input').send_keys(place + Keys.ENTER)
-        time.sleep(10)
-
+        self.search_result_ammount = self.driver.find_element_by_xpath('//*[@id="main-content"]/div/section/div[2]/h1/span[1]').text
+        self.JobLinkList = []
+        with open('./results/scraping/results.txt', 'w') as result_file:
+            for ii in range(1, 20):
+                url = self.driver.find_element_by_xpath('//*[@id="main-content"]/div/section/ul/li[' + str(ii) + ']/a').get_attribute('href')
+                self.JobLinkList.append(url)
+                result_file.write(url + '\n')
+        
